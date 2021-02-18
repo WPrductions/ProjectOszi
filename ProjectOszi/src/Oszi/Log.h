45ecd269140e1c2ed500cdd3e4bf6e55cdd87ac6
WPrductions <markus.radike@gmail.com>
2021-02-18 17:37:21 +0100

    Logging addet

---
+++ ProjectOszi/src/Oszi/Log.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Oszi {
+	
+	class OSZI_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetLogger() { return s_Logger; }
+
+	private:	
+		static std::shared_ptr<spdlog::logger> s_Logger;
+	};
+}
+
+//Console Log Macros
+#define OZ_FATAL(...) ::Oszi::Log::GetLogger()->fatal(__VA_ARGS__)
+#define OZ_ERROR(...) ::Oszi::Log::GetLogger()->error(__VA_ARGS__)
+#define OZ_WARN(...) ::Oszi::Log::GetLogger()->warn(__VA_ARGS__)
+#define OZ_INFO(...) ::Oszi::Log::GetLogger()->info(__VA_ARGS__)
+#define OZ_TRACE(...) ::Oszi::Log::GetLogger()->trace(__VA_ARGS__)
--- ProjectOszi/src/Oszi/Program.h
+++ ProjectOszi/src/Oszi/Program.h
@@ -5,9 +5,17 @@
 extern Oszi::Application* Oszi::CreateApplication();
 
 int main(void) {
+
+	Oszi::Log::Init();
+	int a = 5;
+
+	OZ_INFO("Hello");
+	OZ_ERROR("Yeet {0}", a);
+
 	Oszi::Application* program = Oszi::CreateApplication();
 	program->Run();
 	delete program;
 	return 0;
 }
+
 #endif 
